2a7cd685783f46a73f7befb75afd6ce07cb947c9
Charles Goddard <chargoddard@gmail.com>
2023-08-06 20:04:28 -0700

    Patch after loading

--- src/axolotl/utils/models.py
+++ src/axolotl/utils/models.py
@@ -60,21 +60,6 @@ def load_tokenizer(cfg):
     ]:
         tokenizer.pad_token = "<pad>"  # nosec
 
-
-    LOG.debug(f"EOS: {tokenizer.eos_token_id} / {tokenizer.eos_token}")
-    LOG.debug(f"BOS: {tokenizer.bos_token_id} / {tokenizer.bos_token}")
-    LOG.debug(f"PAD: {tokenizer.pad_token_id} / {tokenizer.pad_token}")
-    LOG.debug(f"UNK: {tokenizer.unk_token_id} / {tokenizer.unk_token}")
-
-    if (
-        tokenizer.__class__.__name__
-        in [
-            "LlamaTokenizer",
-            "LlamaTokenizerFast",
-        ]
-    ):
-        tokenizer.pad_token = "<pad>"  # nosec
-
     if tokenizer.__class__.__name__ == "GPTNeoXTokenizerFast":
         tokenizer.add_special_tokens({"pad_token": "[PAD]"})
         os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -355,6 +340,48 @@ def load_model(
         model.config.num_hidden_layers += int(cfg.llama_hats)
         for _ in range(int(cfg.llama_hats)):
             model.model.layers.append(LlamaDecoderLayer(model.config))
+
+    if cfg.is_llama_derived_model and cfg.flash_attention:
+        if cfg.device not in ["mps", "cpu"] and not cfg.inference:
+            from axolotl.monkeypatch.llama_attn_hijack_flash import (
+                replace_llama_attn_with_flash_attn,
+            )
+
+            LOG.info("patching with flash attention")
+            replace_llama_attn_with_flash_attn()
+    elif cfg.is_llama_derived_model and cfg.xformers_attention:
+        from axolotl.monkeypatch.llama_attn_hijack_xformers import (
+            hijack_llama_attention,
+        )
+
+        LOG.info("patching with xformers attention")
+        hijack_llama_attention()
+    elif cfg.is_llama_derived_model and cfg.sdp_attention:
+        from axolotl.monkeypatch.llama_attn_hijack_xformers import (
+            hijack_llama_sdp_attention,
+        )
+
+        LOG.info("patching with sdp attention")
+        hijack_llama_sdp_attention()
+    elif cfg.is_llama_derived_model and cfg.landmark_attention:
+        from axolotl.monkeypatch.llama_landmark_attn import (
+            MEM_TOKEN,
+            patch_llama_with_landmark_attn,
+        )
+
+        LOG.info("patching with landmark attention")
+        patch_llama_with_landmark_attn()
+
+        # Note: This might overwrite previous additional_special_tokens
+        tokenizer.add_special_tokens({"additional_special_tokens": [MEM_TOKEN]})
+
+    if cfg.is_llama_derived_model and cfg.xpos_rope:
+        from axolotl.monkeypatch.xpos_rope_llama_monkey_patch import (
+            replace_llama_rope_with_xpos_rope,
+        )
+
+        LOG.info("patching with xpos rope")
+        replace_llama_rope_with_xpos_rope()
 
     tl_no_pad = len(tokenizer)
     # tl_no_pad = (
